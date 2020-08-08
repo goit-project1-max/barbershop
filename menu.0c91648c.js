54e01f894f0c8437f308c7fb714d1affde5d7cad
Alena <alena.senkiv@gmail.com>
2020-08-08 13:21:06 +0300

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -121,22 +121,13 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 (function () {
   var menuBtnRef = document.querySelector('[data-menu-button]');
   var mobileMenuRef = document.querySelector('[data-menu]');
-  var body = document.querySelector('body');
-
-  var toggleMenu = function toggleMenu() {
+  var mobileBackdropRef = document.querySelector('[data-backdrop]');
+  menuBtnRef.addEventListener('click', function () {
     var expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
     menuBtnRef.classList.toggle('is-open');
-    body.classList.toggle('scroll-hidden');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
-  };
-
-  menuBtnRef.addEventListener('click', function () {
-    toggleMenu();
-  });
-  mobileMenuRef.addEventListener('click', function (_ref) {
-    var target = _ref.target;
-    target.getAttribute('data-menu-link') === '' && toggleMenu();
+    mobileBackdropRef.classList.toggle('is-open');
   });
 })();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -167,7 +158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58586" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59134" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
