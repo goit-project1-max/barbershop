03f74155b53e7ed3960807e45093b4036f2e6c50
Alena <alena.senkiv@gmail.com>
2020-08-11 19:27:37 +0300

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -118,77 +118,6 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/menu.js":[function(require,module,exports) {
-// (() => {
-//   const refs = {
-//     menuBtn: document.querySelector('[data-menu-button]'),
-//     goLink: document.querySelector('[data-menu-link]'),
-//     // closeBtn2: document.querySelector('[data-menu-close-2]'),
-//     // closeBtn3: document.querySelector('[data-menu-close-3]'),
-//     // closeBtn4: document.querySelector('[data-menu-close-4]'),
-//     // closeBtn5: document.querySelector('[data-menu-close-5]'),
-//     mobileMenu: document.querySelector('[data-backdrop]'),
-//     menuInner: document.querySelector('[data-menu-inner]'),
-//   };
-//   refs.menuBtn.addEventListener('click', () => {
-//     const expanded = refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
-//     refs.menuBtn.classList.toggle('is-open');
-//     refs.menuBtn.setAttribute('aria-expanded', !expanded);
-//     refs.mobileMenu.classList.toggle('is-visible');
-//     refs.menuInner.classList.toggle('is-open');
-//     document.body.classList.toggle('modal-open');
-//   });
-//   refs.goLink.addEventListener('click', () => {
-//     const expanded = refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
-//     refs.menuBtn.classList.toggle('is-open');
-//     refs.menuBtn.setAttribute('aria-expanded', !expanded);
-//     refs.mobileMenu.classList.toggle('is-visible');
-//     refs.menuInner.classList.toggle('is-open');
-//     document.body.classList.toggle('modal-open');
-//   });
-//   // refs.closeBtn2.addEventListener('click', () => {
-//   //   const expanded = refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
-//   //   refs.menuBtn.classList.toggle('is-open');
-//   //   refs.menuBtn.setAttribute('aria-expanded', !expanded);
-//   //   refs.mobileMenu.classList.toggle('is-visible');
-//   //   refs.sideBar.classList.toggle('is-open');
-//   //   document.body.classList.toggle('modal-open');
-//   // });
-//   // refs.closeBtn3.addEventListener('click', () => {
-//   //   const expanded = refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
-//   //   refs.menuBtn.classList.toggle('is-open');
-//   //   refs.menuBtn.setAttribute('aria-expanded', !expanded);
-//   //   refs.mobileMenu.classList.toggle('is-visible');
-//   //   refs.sideBar.classList.toggle('is-open');
-//   //   document.body.classList.toggle('modal-open');
-//   // });
-//   // refs.closeBtn4.addEventListener('click', () => {
-//   //   const expanded = refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
-//   //   refs.menuBtn.classList.toggle('is-open');
-//   //   refs.menuBtn.setAttribute('aria-expanded', !expanded);
-//   //   refs.mobileMenu.classList.toggle('is-visible');
-//   //   refs.sideBar.classList.toggle('is-open');
-//   //   document.body.classList.toggle('modal-open');
-//   // });
-//   // refs.closeBtn5.addEventListener('click', () => {
-//   //   const expanded = refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
-//   //   refs.menuBtn.classList.toggle('is-open');
-//   //   refs.menuBtn.setAttribute('aria-expanded', !expanded);
-//   //   refs.mobileMenu.classList.toggle('is-visible');
-//   //   refs.sideBar.classList.toggle('is-open');
-//   //   document.body.classList.toggle('modal-open');
-//   // });
-// })();
-// // const anchors = document.querySelectorAll('a[href*="#"]');
-// // for (let anchor of anchors) {
-// //   anchor.addEventListener('click', function (e) {
-// //     e.preventDefault();
-// //     const blockID = anchor.getAttribute('href').substr(1);
-// //     document.getElementById(blockID).scrollIntoView({
-// //       behavior: 'smooth',
-// //       block: 'start',
-// //     });
-// //   });
-// // }
 (function () {
   var menuBtnRef = document.querySelector('[data-menu-button]');
   var mobileMenuRef = document.querySelector('[data-menu-inner]');
@@ -240,7 +169,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52147" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62059" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
