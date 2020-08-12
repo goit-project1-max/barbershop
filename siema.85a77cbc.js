a95c52c2ec582ae99237a4cfa40d7248bca2d516
Alena <alena.senkiv@gmail.com>
2020-08-12 15:46:38 +0300

    Updates

--- siema.85a77cbc.js
+++ siema.85a77cbc.js
@@ -127,28 +127,6 @@ var _siema = _interopRequireDefault(require("siema"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
 var mySiema = new _siema.default({
   selector: '.siema',
   duration: 500,
@@ -172,91 +150,58 @@ document.querySelector('.prev').addEventListener('click', function () {
 });
 document.querySelector('.next').addEventListener('click', function () {
   return mySiema.next();
-}); // extend a Siema class by two methods
-// addDots - to create a markup for dots
-// updateDots - to update classes on dots on change callback
-
-var SiemaWithDots = /*#__PURE__*/function (_Siema) {
-  _inherits(SiemaWithDots, _Siema);
-
-  var _super = _createSuper(SiemaWithDots);
-
-  function SiemaWithDots() {
-    _classCallCheck(this, SiemaWithDots);
-
-    return _super.apply(this, arguments);
-  }
-
-  _createClass(SiemaWithDots, [{
-    key: "addDots",
-    value: function addDots() {
-      var _this = this;
-
-      // create a contnier for all dots
-      // add a class 'dots' for styling reason
-      this.dots = document.createElement('div');
-      this.dots.classList.add('dots'); // loop through slides to create a number of dots
-
-      var _loop = function _loop(i) {
-        // create a dot
-        var dot = document.createElement('button'); // add a class to dot
-
-        dot.classList.add('dots__item'); // add an event handler to each of them
-
-        dot.addEventListener('click', function () {
-          _this.goTo(i);
-        }); // append dot to a container for all of them
-
-        _this.dots.appendChild(dot);
-      };
-
-      for (var i = 0; i < this.innerElements.length; i++) {
-        _loop(i);
-      } // add the container full of dots after selector
-
-
-      this.selector.parentNode.insertBefore(this.dots, this.selector.nextSibling);
-    }
-  }, {
-    key: "updateDots",
-    value: function updateDots() {
-      // loop through all dots
-      for (var i = 0; i < this.dots.querySelectorAll('button').length; i++) {
-        // if current dot matches currentSlide prop, add a class to it, remove otherwise
-        var addOrRemove = this.currentSlide === i ? 'add' : 'remove';
-        this.dots.querySelectorAll('button')[i].classList[addOrRemove]('dots__item--active');
-      }
-    }
-  }]);
-
-  return SiemaWithDots;
-}(_siema.default); // instantiate new extended Siema
-
-
-var mySiemaWithDots = new SiemaWithDots({
-  // on init trigger method created above
-  onInit: function onInit() {
-    this.addDots();
-    this.updateDots();
-  },
-  // on change trigger method created above
-  onChange: function onChange() {
-    this.updateDots();
-  }
-}); // Siema doesn't come with pagination built in
-// But it is very easy to add one if you want
-// // New siema instance
-// // Add a function that generates pagination to prototype
-// Siema.prototype.addPagination = function () {
-//   for (let i = 0; i < this.innerElements.length; i++) {
-//     const btn = document.createElement('button');
-//     btn.textContent = i;
-//     btn.addEventListener('click', () => this.goTo(i));
-//     this.selector.appendChild(btn);
+}); //
+//
+//
+//
+//
+//
+// // extend a Siema class by two methods
+// // addDots - to create a markup for dots
+// // updateDots - to update classes on dots on change callback
+// class SiemaWithDots extends Siema {
+//   addDots() {
+//     // create a contnier for all dots
+//     // add a class 'dots' for styling reason
+//     this.dots = document.createElement('div');
+//     this.dots.classList.add('dots');
+//     // loop through slides to create a number of dots
+//     for (let i = 0; i < this.innerElements.length; i++) {
+//       // create a dot
+//       const dot = document.createElement('button');
+//       // add a class to dot
+//       dot.classList.add('dots__item');
+//       // add an event handler to each of them
+//       dot.addEventListener('click', () => {
+//         this.goTo(i);
+//       });
+//       // append dot to a container for all of them
+//       this.dots.appendChild(dot);
+//     }
+//     // add the container full of dots after selector
+//     this.selector.parentNode.insertBefore(this.dots, this.selector.nextSibling);
 //   }
-// };
-// // Trigger pagination creator
-// mySiema.addPagination();
+//   updateDots() {
+//     // loop through all dots
+//     for (let i = 0; i < this.dots.querySelectorAll('button').length; i++) {
+//       // if current dot matches currentSlide prop, add a class to it, remove otherwise
+//       const addOrRemove = this.currentSlide === i ? 'add' : 'remove';
+//       this.dots.querySelectorAll('button')[i].classList[addOrRemove]('dots__item--active');
+//     }
+//   }
+// }
+// // instantiate new extended Siema
+// const mySiemaWithDots = new SiemaWithDots({
+//   // on init trigger method created above
+//   onInit: function () {
+//     this.addDots();
+//     this.updateDots();
+//   },
+//   // on change trigger method created above
+//   onChange: function () {
+//     this.updateDots();
+//   },
+// });
 },{"siema":"../node_modules/siema/dist/siema.min.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -285,7 +230,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62059" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58407" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
